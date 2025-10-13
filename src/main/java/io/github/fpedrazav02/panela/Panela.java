9bfb04548c9a92b8278bfd2184f48944715ce4bb
Pedraza Vázquez, Francisco <fpedrazav02@gmail.com>
2025-10-13 22:42:08 +0200

    feat(MAC): Include native compilation for MacOs

--- src/main/java/io/github/fpedrazav02/panela/Panela.java
+++ src/main/java/io/github/fpedrazav02/panela/Panela.java
@@ -6,13 +6,13 @@ import picocli.CommandLine.Command;
 /** App */
 @Command(name = "panela", description = "Panela - A lightweight and extendable ETL runner")
 public class Panela implements Runnable {
-  public static void main(String[] args) {
-    int exitCode = new CommandLine(new Panela()).execute(args);
-    System.exit(exitCode);
-  }
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Panela()).execute(args);
+        System.exit(exitCode);
+    }
 
-  @Override
-  public void run() {
-    System.out.println("Panela CLI - use 'panela init <name>' to start a new job.");
-  }
+    @Override
+    public void run() {
+        System.out.println("Panela CLI - use 'panela init <name>' to start a new job.");
+    }
 }
