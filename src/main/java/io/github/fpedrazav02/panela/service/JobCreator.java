db9e2f06ea8e5c2ce2834832f6b998b3aca386ab
Pedraza Vázquez, Francisco <fpedrazav02@gmail.com>
2025-11-23 22:52:37 +0100

    feat(POML): Remove ugly -jar-with-dependencies
    - Missed actual AssemblyId field

--- src/main/java/io/github/fpedrazav02/panela/service/JobCreator.java
+++ src/main/java/io/github/fpedrazav02/panela/service/JobCreator.java
@@ -6,8 +6,10 @@ import io.github.fpedrazav02.panela.utils.Result;
 import io.github.fpedrazav02.panela.validator.impl.JobNameValidator;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class JobCreator {
     private final PanelaHome panelaHome;
@@ -21,7 +23,9 @@ public class JobCreator {
     }
 
     public void createJob(String jobName) throws IOException, InvalidJobName {
+        // Check validations
         Result<String> validation = new JobNameValidator(jobName).validate();
+        // TODO: Include exists validation ¿?! DBs + Providers + Local
 
         if (!validation.isSuccess()) {
             throw new InvalidJobName(validation.getError().orElse("Invalid Job Name"));
@@ -39,19 +43,38 @@ public class JobCreator {
         Files.createDirectories(luaSdkDir);
         Files.createDirectories(runtimeDir);
 
-        //Copy SDK Lua files to Job
-        //this.copyResourcesToFolder();
-
+        // Copy SDK Lua files
+        this.copyResourcesToFolder("templates.job/meta/lua-sdk/job.d.lua", luaSdkDir.resolve("job.d.lua"));
         //Copy runtime files to Job
+        this.copyResourcesToFolder("templates.job/meta/runtime/job.lua", runtimeDir.resolve("job.lua"));
 
+        // TODO: Pass this to a file
+        String jobContent = """
+                local job = require("job")
+                
+                return job.define {
+                  name = "%s",
+                  version = "0.1.0",
+                  inputs = {},
+                  transforms = {},
+                  outputs = {}
+                }
+                """.formatted(jobName);
+        Files.writeString(jobDir.resolve("job.lua"), jobContent);
 
         // TODO: DB or file registration
-
     }
 
     private void copyResourcesToFolder(String resourcePath, Path destinationFolder) throws IOException {
+        // Since it is a JAR compilation an inputStream is needed + try-with-resources cleans the stream
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
+            if (inputStream == null) {
+                throw new IOException("Resource not found: " + resourcePath);
+            }
+
+            Files.createDirectories(destinationFolder);
+            Files.copy(inputStream, destinationFolder.resolve(resourcePath), StandardCopyOption.REPLACE_EXISTING);
+        }
 
     }
-
-
 }
